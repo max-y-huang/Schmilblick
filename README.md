3f8bf1cfcdded2d64625843d17cf40ffa3e25b95
max-y-huang <letmaxknow@gmail.com>
2024-05-27 01:38:58 -0400

    modularized parser code, normalized tempo and divisions

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 **Input (Sample data [here](https://drive.google.com/drive/folders/19GXoGG40P6MN9dCoI2gPH88HKLXORbpS?usp=drive_link)):**
 
-- `/in/score.mxl`: a compressed MusicXML file.
+- `/in/<TEST_CASE>/score.mxl`: a compressed MusicXML file.
 
 **Output:**
 
-- `/out/notes.json`: a list of note information containing pitch, start, and duration data.
-- (optional) `/out/preview.midi`: a MIDI file built from `/out/notes.json`.
+- `/out/<TEST_CASE>/notes.json`: a list of note information containing pitch, start, and duration data.
+- (optional) `/out/<TEST_CASE>/preview.midi`: a MIDI file built from `/out/<TEST_CASE>/notes.json`.
--- etl/parse_mxl.py
+++ etl/parse_mxl.py
@@ -11,29 +11,137 @@ import tempfile
 from xml.etree import ElementTree
 
 
-_IN_MXL_DIR = './in/score.mxl'
-_OUT_MIDI_DIR = './out/preview_<ID>.mid'
-_OUT_DATA_DIR = './out/data_<ID>.json'
+_IN_MXL_DIR = './in/<TEST_CASE>/score.mxl'
+_OUT_MIDI_DIR = './out/<TEST_CASE>/preview_<PART>.mid'
+_OUT_DATA_DIR = './out/<TEST_CASE>/data_<PART>.json'
 
 
-class Note:
-    def __init__(self, start, duration, pitch):
-        self.start = start
-        self.duration = duration
-        self.pitch = pitch
+class Event:
+    def __init__(self, time, data):
+        self.time = time
+        self.data = data
     
     def as_json(self):
-        return { 'start': self.start, 'duration': self.duration, 'pitch': self.pitch }
+        return { 'time': self.time, 'data': self.data }
+
+
+class ParserBase:
+    class ParseState:
+        @property
+        def time(self):
+            return self._time
+        
+        @time.setter
+        def time(self, value):
+            self.prev_time = self._time
+            self._time = value
+
+        def __init__(self):
+            self.acc = []
+            self._time = 0
+            self.tempo = 108 * 2
+            self.divisions = 1
+        
+        def normalize_duration(self, duration):
+            beats_per_minute = self.tempo
+            divisions_per_beat = self.divisions
+            minutes_per_second = 1 / 60
+            mult = 1 / (beats_per_minute * divisions_per_beat * minutes_per_second)  # in seconds per division
+            return duration * mult
+
+    def __init__(self, data):
+        self.data = data
+    
+    def parse(self):
+        state = self.ParseState()
+        for obj, obj_type in self.get_objects(self.data):
+            eval(f'self.handle_{obj_type}')(state, obj)
+        return state.acc
+
+    def get_objects(self, obj):
+        objs_to_parse = {**self.objects_to_parse, **self.default_objects_to_parse}
+        obj_type = next((key for key, val in objs_to_parse.items() if val(obj)), None)  # get matching type of interest
+        if obj_type is not None:
+            return [(obj, obj_type)]
+        ret = []
+        for child in obj:
+            ret += self.get_objects(child)
+        return ret
+
+    ################################
+    
+    default_objects_to_parse = {
+        'divisions': lambda x: x.tag == 'divisions',
+        'tempo': lambda x: x.tag == 'sound' and 'tempo' in x.attrib,
+        'backup': lambda x: x.tag == 'backup',
+        'duration': lambda x: x.tag == 'duration'
+    }
+
+    def handle_divisions(self, state, obj):
+        state.divisions = int(obj.text.strip())
+
+    def handle_tempo(self, state, obj):
+        state.tempo = float(obj.get('tempo').strip())
+
+    def handle_backup(self, state, obj):
+        duration = state.normalize_duration(int(obj.find('duration').text.strip()))
+        state.time -= duration
+
+    def handle_duration(self, state, obj):
+        duration = state.normalize_duration(int(obj.text.strip()))
+        state.time += duration
+
+
+class PartParser(ParserBase):
+
+    def parse(self):
+        notes = super().parse()
+        merged_notes = []
+        last_note_by_pitch = {}
+        for note, is_tied in notes:
+            pitch = note.data['pitch']
+            if is_tied and pitch in last_note_by_pitch:  # TODO: why do we need to check "pitch in last_note_by_pitch"?
+                last_note_by_pitch[pitch].data['duration'] += note.data['duration']
+            else:
+                last_note_by_pitch[pitch] = note
+                merged_notes.append(note)
+        return merged_notes
+
+    objects_to_parse = {
+        'note': lambda x: x.tag == 'note',
+    }
+    
+    def handle_note(self, state, obj):
+        # get note information
+        is_grace_note = obj.find('grace') is not None
+        is_chord = obj.find('chord') is not None
+        is_pitched = obj.find('pitch') is not None
+        is_rest = obj.find('rest') is not None
+        is_tied = obj.find('.//tie[@type!="start"]') is not None
+        # skip grace notes
+        if is_grace_note:
+            return
+        duration = state.normalize_duration(float(obj.find('duration').text.strip()))
+        # handle start time for chord notes
+        if is_chord:
+            state.time = state.prev_time  # go to start time of previous note
+        # save pitched notes
+        if not is_rest and is_pitched:
+            pitch = pitch_xml_to_int(obj.find('pitch'))
+            state.acc.append((Event(state.time, {'duration': duration, 'pitch': pitch}), is_tied))
+        # use up note duration
+        state.time += duration
 
 
 def import_mxl_as_xml(dir):
-    _UNCOMPRESSED_SCORE_DIR = 'score.xml'
     # unzip file to temporary directory
-    with zipfile.ZipFile(dir, 'r') as zip:
+    with zipfile.ZipFile(dir, 'r') as z:
         zip_dir = tempfile.mkdtemp()
-        zip.extractall(zip_dir)
+        z.extractall(zip_dir)
+        xml_fname = next(d for d in os.listdir(zip_dir) if d.endswith('.xml'))  # get first .xml file in zip_dir
+        xml_dir = os.path.join(zip_dir, xml_fname)
     # get xml from temporary directory
-    with open(os.path.join(zip_dir, _UNCOMPRESSED_SCORE_DIR)) as f:
+    with open(xml_dir) as f:
         obj = ElementTree.parse(f)
     return obj
 
@@ -46,84 +154,44 @@ def pitch_xml_to_int(obj):
         alter = int(obj.find('alter').text.strip())
     except:
         alter = 0
-    return octave * 12 + _STEP_OFFSET[step] + alter
+    return 12 + octave * 12 + _STEP_OFFSET[step] + alter
 
 
-def parse_xml_part(obj):
-    def parse_notes(obj):
-        notes = []
-        prev_time, time = 0, 0
-        for note_obj in obj.findall('.//note'):
-            # get note information
-            is_grace_note = note_obj.find('grace') is not None
-            is_chord = note_obj.find('chord') is not None
-            is_pitched = note_obj.find('pitch') is not None
-            is_rest = note_obj.find('rest') is not None
-            is_tied = note_obj.find('.//tie[@type!="start"]') is not None
-            # skip grace notes
-            if is_grace_note:
-                continue
-            # handle start time for chord notes
-            if is_chord:
-                time = prev_time
-            # get duration
-            duration = int(note_obj.find('duration').text.strip())
-            # save pitched notes
-            if not is_rest and is_pitched:
-                pitch = pitch_xml_to_int(note_obj.find('pitch'))
-                notes.append((Note(time, duration, pitch), is_tied))
-            # use up note duration
-            prev_time = time
-            time += duration
-        return notes
-    
-    def merge_tied_notes(notes):
-        merged_notes = []
-        last_note_by_pitch = {}
-        for note, is_tied in notes:
-            if is_tied:
-                last_note_by_pitch[note.pitch].duration += note.duration
-            else:
-                last_note_by_pitch[note.pitch] = note
-                merged_notes.append(note)
-        return merged_notes
-    
-    return merge_tied_notes(parse_notes(obj))
-
-
-def save_notes_as_json(notes, part, id):
-    with open(_OUT_DATA_DIR.replace('<ID>', id), 'w') as f:
+def save_notes_as_json(notes, test_case, part_name, part_id):
+    with open(_OUT_DATA_DIR.replace('<TEST_CASE>', test_case).replace('<PART>', part_id), 'w') as f:
         note_data = [ note.as_json() for note in notes ]
-        data = { 'part': part, 'notes': note_data }
+        data = { 'part': part_name, 'notes': note_data }
         print(json.dumps(data), file=f)
 
 
-def save_notes_as_midi(notes, id):
+def save_notes_as_midi(notes, test_case, part_id):
     track, channel = 0, 0
-    MyMIDI = midiutil.MIDIFile(1)
-    MyMIDI.addTempo(track, channel, 108 * 12)
+    MyMIDI = midiutil.MIDIFile(1, deinterleave=False)
+    MyMIDI.addTempo(track, channel, 60)  # 60 bpm = 1 beat per second
     for note in notes:
-        if type(note) is Note:
-            MyMIDI.addNote(track, channel, note.pitch, note.start, note.duration, 127)
-    with open(_OUT_MIDI_DIR.replace('<ID>', id), 'wb') as f:
+        if type(note) is Event:
+            MyMIDI.addNote(track, channel, note.data['pitch'], note.time, note.data['duration'], 127)
+    with open(_OUT_MIDI_DIR.replace('<TEST_CASE>', test_case).replace('<PART>', part_id), 'wb') as f:
         MyMIDI.writeFile(f)
 
 
 if __name__ == '__main__':
 
     arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-t', '--test_case', help='the test case to use', required=True)
     arg_parser.add_argument('-p', '--preview', help='the part to preview in MIDI form')
     args = arg_parser.parse_args()
 
-    pathlib.Path('./out').mkdir(parents=True, exist_ok=True) 
+    test_case = args.test_case
+    pathlib.Path(f'./out/{test_case}').mkdir(parents=True, exist_ok=True) 
 
-    score_obj = import_mxl_as_xml(_IN_MXL_DIR)
+    score_obj = import_mxl_as_xml(_IN_MXL_DIR.replace('<TEST_CASE>', test_case))
     for part_obj in score_obj.findall('part'):
         part_id = part_obj.get('id')
         part_name = score_obj.find(f'.//score-part[@id="{part_id}"]/part-name').text
-        notes = parse_xml_part(part_obj)
-        save_notes_as_json(notes, part_name, part_id)
-        save_notes_as_midi(notes, part_id)
+        notes = PartParser(part_obj).parse()
+        save_notes_as_json(notes, test_case, part_name, part_id)
+        save_notes_as_midi(notes, test_case, part_id)
     
     if args.preview:
-        os.system(f'start {_OUT_MIDI_DIR.replace("<ID>", args.preview)}')
+        os.system(f'start {_OUT_MIDI_DIR.replace("<TEST_CASE>", test_case).replace("<PART>", args.preview)}')
