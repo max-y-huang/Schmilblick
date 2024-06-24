4e713184f385d1c0c186494d7f345520d77825e0
Nguyen Tran <nguyen.tran1072e@gmail.com>
2024-06-24 18:06:03 -0400

    Display PDF for the first time

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,19 +1,86 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-// import 'package:record_example/audio_player.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'audio_recorder.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: ScoreSheet(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class ScoreSheet extends StatefulWidget {
+  const ScoreSheet({super.key});
+
+  @override
+  State<ScoreSheet> createState() => _ScoreSheetState();
+}
+
+class _ScoreSheetState extends State<ScoreSheet> {
+  Uint8List? _pdfbytes;
+  late final PDFViewController _pdfViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    setFile();
+  }
+
+  void setFile() async {
+    final file = await rootBundle.load('assets/happy_birthday.pdf');
+    setState(() {
+      _pdfbytes = file.buffer.asUint8List();
+    });
+  }
+
+  void goToLastPage() async {
+    final pageCount = await _pdfViewController.getPageCount();
+    print(pageCount);
+    if (pageCount != null) {
+      await _pdfViewController.setPage(pageCount - 1);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: _pdfbytes != null ? PDFView(
+        pdfData: _pdfbytes!,
+        swipeHorizontal: true,
+        onError: (error) {
+          print(error.toString());
+        },
+        onViewCreated: (PDFViewController pdfViewController) {
+          _pdfViewController = pdfViewController;
+          goToLastPage();
+        },
+      ) : Placeholder(),
+    );
+  }
+}
+
+class RecorderPage extends StatefulWidget {
+  const RecorderPage({ super.key }); 
+
+  @override
+  State<RecorderPage> createState() => _RecorderPage();
+}
+
+class _RecorderPage extends State<RecorderPage> {
   bool showPlayer = false;
   String? audioPath;
 
@@ -25,18 +92,16 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Recorder(
-            onStop: (path) {
-              if (kDebugMode) print('Recorded file path: $path');
-              setState(() {
-                audioPath = path;
-                showPlayer = true;
-              });
-            },
-          ),
+    return Scaffold(
+      body: Center(
+        child: Recorder(
+          onStop: (path) {
+            if (kDebugMode) print('Recorded file path: $path');
+            setState(() {
+              audioPath = path;
+              showPlayer = true;
+            });
+          },
         ),
       ),
     );
