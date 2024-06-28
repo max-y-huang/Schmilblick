02683d7e6b7923cfdc7c72ebfe36b4b5cf55e393
Nguyen Tran <nguyen.tran1072e@gmail.com>
2024-06-28 11:45:12 -0400

    Demonstrated that we can parse the SVG XML

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:ui';
 import 'dart:async';
 import 'package:flutter/foundation.dart';
@@ -7,6 +8,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:archive/archive_io.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:xml/xml.dart';
 import 'audio_recorder.dart';
 import 'dart:math';
 
@@ -23,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: PagedScoreSheet(),
+      home: ContinuousScoreSheet(),
     );
   }
 }
@@ -37,11 +39,12 @@ class ContinuousScoreSheet extends StatefulWidget {
 
 class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
   final uri = 'http://localhost:3000'; // Replace this with localhost.run uri
-  int? _width;
+  late int _width;
   List<SvgPicture>? _svgs;
+  List<XmlDocument>? _svgXml;
 
   late final ScrollController _scrollController;
-  late final Timer _timer;
+  // late final Timer _timer;
 
   Future<List<OutputStream>> _getSvgLinks(int imageWidth) async {
     final request = http.MultipartRequest('POST', Uri.parse('$uri/musicxml-to-svg'));
@@ -67,45 +70,41 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
     return outputStreams;
   }
 
-  void _getSvg() async {
-    if (_width == null) {
-      return;
+  void _setupSvg() async {
+    List<OutputStream> outputStreams = await _getSvgLinks(_width);
+    List<SvgPicture> svgPictures = [];
+    List<XmlDocument> svgDocuments = [];
+
+    for (final stream in outputStreams) {
+      final XmlDocument document = XmlDocument.parse(utf8.decode(stream.getBytes()));
+      final SvgPicture picture = SvgPicture.memory(stream.getBytes() as Uint8List);
+
+      svgPictures.add(picture);
+      svgDocuments.add(document);
     }
 
-    List<OutputStream> outputStreams = await _getSvgLinks(_width!);
-    final List<SvgPicture> outputPictures = outputStreams.map((stream) => SvgPicture.memory(stream.getBytes() as Uint8List)).toList();
     setState(() {
-      _svgs = outputPictures;
+      _svgs = svgPictures;
     });
+    _svgXml = svgDocuments;
   }
 
-  void handleAttach(ScrollPosition scrollposition) {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      final offset = _scrollController.offset;
-      _scrollController.jumpTo(offset + 10);
-    });
+  void _setWidth() {
+    FlutterView view = WidgetsBinding.instance.platformDispatcher.views.first;
+    Size size = view.physicalSize / view.devicePixelRatio;
+    _width = size.width.toInt();
   }
 
-  void handleDetach(ScrollPosition scrollposition) {
-    _timer.cancel();
+  void _getMeasureInfo() {
+    
   }
 
   @override
   void initState() {
     super.initState();
-    // First get the FlutterView.
-    FlutterView view = WidgetsBinding.instance.platformDispatcher.views.first;
-    
-    // Dimensions in physical pixels (px)
-    Size size = view.physicalSize / view.devicePixelRatio;
-    _width = size.width.toInt();
-
-    _getSvg();
-
-    _scrollController = ScrollController(
-      onAttach: handleAttach,
-      onDetach: handleDetach
-    );
+    _setWidth();
+    _setupSvg();
+    _scrollController = ScrollController();
   }
 
   @override
