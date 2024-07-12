a1ff8628967301d24a0d89c3537e7e3f0e889d37
Nguyen Tran <nguyen.tran1072e@gmail.com>
2024-07-12 14:30:21 -0400

    Implement paged mode score sheet

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
 import 'audio_recorder.dart';
-import 'dart:math';
 
 void main() => runApp(const MyApp());
 
@@ -20,69 +22,113 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: ScoreSheet(),
+      home: const PagedScoreSheet(),
     );
   }
 }
 
-class ScoreSheet extends StatefulWidget {
-  const ScoreSheet({super.key});
+class PagedScoreSheet extends StatefulWidget {
+  const PagedScoreSheet({super.key});
 
   @override
-  State<ScoreSheet> createState() => _ScoreSheetState();
+  State<PagedScoreSheet> createState() => _PagedScoreSheetState();
 }
 
-class _ScoreSheetState extends State<ScoreSheet> {
-  Uint8List? _pdfBytes;
+class _PagedScoreSheetState extends State<PagedScoreSheet> {
+  // When running this app, use the url provided by localhost.run
+  final uri = "http://localhost:4000"; // TODO: We will need to remove this at some point
+  final score = "emerald_moonlight";
+  late final Future<Uint8List> _pdfBytes;
   late final PDFViewController _pdfViewController;
-  late int? _pageCount;
-  late Timer _timer;
+  late final List<int> _pageTable;
+
+  int _randomMeasure = 0;
+  final Random rand = Random();
 
   @override
   void initState() {
     super.initState();
-    setFile();
+    _extractCompiledMxlInformation();
+    _getFile(); 
   }
 
-  void setFile() async {
-    // TODO: Turn the file into a backend call. For now, as proof of concept
-    // we'll use this file for now.
-    final file = await rootBundle.load('assets/joplin-scott-the-entertainer.pdf');
+  void _getFile() async {
+    // TODO: Turn the file into a backend call.
+    final pdfBytes = rootBundle.load('assets/$score.pdf').then((file) => file.buffer.asUint8List());
     setState(() {
-      _pdfBytes = file.buffer.asUint8List();
+      _pdfBytes = pdfBytes;
     });
   }
 
-  Future<void> goToRandomPagePerSecond(PDFViewController pdfViewController) async {
-    var rng = Random();
-    _pdfViewController = pdfViewController;
+  Future<http.Response> _compileMxl() async {
+    final request = http.MultipartRequest('POST', Uri.parse('$uri/compile-mxl'));
 
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
-      // TODO: Replace this function and comment. As proof of concept, you
-      // can programmatically change the page with the PDFViewController
-      _pageCount = await _pdfViewController.getPageCount();
-      if (_pageCount != null && _pageCount! > 0) {
-        final page = rng.nextInt(_pageCount!);
-        await _pdfViewController.setPage(page);
-      }
-    });
+    final musicxmlBytes = (await rootBundle.load('assets/$score.mxl')).buffer.asUint8List();
+    request.files.add(http.MultipartFile.fromBytes('file', musicxmlBytes, filename: score));
+
+    final streamResponse = await request.send();
+    final response = await http.Response.fromStream(streamResponse);
+
+    return response;
+  }
+
+  void _extractCompiledMxlInformation() async {
+    final response = await _compileMxl();
+    final resJson = jsonDecode(response.body) as Map<String, dynamic>;
+    final parts = resJson["parts"] as Map<String, dynamic>;
+    final firstPart = parts.keys.first;
+    final pageTableDyn = parts[firstPart]["page_table"] as List<dynamic>;
+    final pageTable = pageTableDyn.map((e) => e as int).toList();
+    _pageTable = pageTable;
+  }
+
+  void _initializeController(PDFViewController pdfViewController) {
+    _pdfViewController = pdfViewController;
+  }
+
+  void jumpToMeasure(int measureId) {
+    if (0 <= measureId && measureId < _pageTable.length) {
+      final int pageNumber = _pageTable[measureId];
+      _pdfViewController.setPage(pageNumber);
+    }
   }
 
   @override
   void dispose() {
     super.dispose();
-    _timer.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: _pdfBytes != null ? PDFView(
-        pdfData: _pdfBytes!,
-        swipeHorizontal: true,
-        onViewCreated: goToRandomPagePerSecond,
-      ) : Placeholder(),
+      child: FutureBuilder(future: _pdfBytes, builder: (buildContext, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            body: PDFView(
+              pdfData: snapshot.data,
+              swipeHorizontal: true,
+              onViewCreated: _initializeController,
+            ),
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: () {
+                jumpToMeasure(_randomMeasure);
+                setState(() {
+                  _randomMeasure = rand.nextInt(_pageTable.length);
+                });
+              },
+              label: Text(
+                '${_randomMeasure + 1}',
+                style: const TextStyle(
+                  fontSize: 60.0,
+                )
+              ),
+            )
+          );
+        } else {
+          return Placeholder();
+        }
+      }) ,
     );
   }
 }
