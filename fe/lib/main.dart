4b3f6879662a2f6d743a0375e84f17da31b0a383
Barry Zhang <45883553+bluebarryz@users.noreply.github.com>
2024-07-12 22:30:37 -0400

    checkpoint - done testing, save prints

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -29,7 +29,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class ContinuousScoreSheet extends StatefulWidget {
   const ContinuousScoreSheet({super.key});
 
@@ -39,9 +38,9 @@ class ContinuousScoreSheet extends StatefulWidget {
 
 class GroupInfo {
   int startingMeasure;
-  double equatorY;
+  double minY;
 
-  GroupInfo(this.startingMeasure, this.equatorY);    
+  GroupInfo(this.startingMeasure, this.minY);
 }
 
 class MinMaxYCoords {
@@ -52,24 +51,27 @@ class MinMaxYCoords {
 }
 
 class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
-  final uri = 'https://464d9801274c1a.lhr.life'; // Replace this with localhost.run uri
+  final uri = 'http://localhost:3000'; // Replace this with localhost.run uri
   late int _width;
   late int _height;
   late Future<SvgPicture> _svgs;
   late Future<XmlDocument> _svgXml;
   var groups = <GroupInfo>[];
-  final RegExp lineCoordinatesRegex = RegExp(r'M(?<x1>[\d\.]+) (?<y1>[\d\.]+)L(?<x2>[\d\.]+) (?<y2>[\d\.]+)$');
-
+  final RegExp lineCoordinatesRegex =
+      RegExp(r'M(?<x1>[\d\.]+) (?<y1>[\d\.]+)L(?<x2>[\d\.]+) (?<y2>[\d\.]+)$');
 
   late final ScrollController _scrollController;
 
   Future<http.Response> _getSvgLinks(int imageWidth) async {
-    final request = http.MultipartRequest('POST', Uri.parse('$uri/musicxml-to-svg'));
+    final request =
+        http.MultipartRequest('POST', Uri.parse('$uri/musicxml-to-svg'));
     request.fields['pageWidth'] = imageWidth.toString();
 
-    const filename = "emerald_moonlight.mxl";
-    final musicxmlBytes = (await rootBundle.load('assets/$filename')).buffer.asUint8List();
-    request.files.add(http.MultipartFile.fromBytes('musicxml', musicxmlBytes, filename: filename));
+    const filename = "viva_la_vida.mxl";
+    final musicxmlBytes =
+        (await rootBundle.load('assets/$filename')).buffer.asUint8List();
+    request.files.add(http.MultipartFile.fromBytes('musicxml', musicxmlBytes,
+        filename: filename));
 
     final streamResponse = await request.send();
     final response = await http.Response.fromStream(streamResponse);
@@ -79,8 +81,10 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
 
   void _setupSvg() {
     final response = _getSvgLinks(_width);
-    final svgDocument = response.then((body) => XmlDocument.parse(utf8.decode(body.bodyBytes)));
-    final svgPicture = response.then((body) => SvgPicture.memory(body.bodyBytes));
+    final svgDocument =
+        response.then((body) => XmlDocument.parse(utf8.decode(body.bodyBytes)));
+    final svgPicture =
+        response.then((body) => SvgPicture.memory(body.bodyBytes));
 
     setState(() {
       _svgs = svgPicture;
@@ -101,39 +105,31 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
   bool _isMeasure(XmlElement elem) {
     if (elem.getAttribute('class') == null) {
       return false;
-    } else if (
-      elem.getAttribute('class')!.contains('vf-measure') &&
-      elem.getAttribute('id') != '-1'
-    ) {
+    } else if (elem.getAttribute('class')!.contains('vf-measure') &&
+        elem.getAttribute('id') != '-1') {
       return true;
     } else {
       return false;
     }
   }
 
-  // Determine if elem is a 'line' element e.g. 
+  // Determine if elem is a 'line' element e.g.
   //  <path stroke-width="1" fill="none" stroke="#000000" stroke-dasharray="none" d="M171.66015625 625.5999999999999L472.4196980046949 625.5999999999999"></path>
   bool _isLine(XmlElement elem) {
     final String? dAttribute = elem.getAttribute("d");
-    if (elem.name.local == "path" && dAttribute != null && lineCoordinatesRegex.hasMatch(dAttribute)) {
+    if (elem.name.local == "path" &&
+        dAttribute != null &&
+        elem.getAttribute("class") == null &&
+        lineCoordinatesRegex.hasMatch(dAttribute)) {
       return true;
     } else {
       return false;
     }
   }
 
-  // Return the first child element of elem for which the predicate returns true
-  XmlElement? _getChildElement(XmlElement elem, bool Function(XmlElement) predicate) {
-    for (final XmlElement child in elem.childElements) {
-      if (predicate(child)) {
-        return child;
-      }
-    }
-    return null;
-  }
-
   // Return all child elements of elem for which the predicate returns true
-  List<XmlElement> _getAllChildElements(XmlElement elem, bool Function(XmlElement) predicate) {
+  List<XmlElement> _getAllChildElements(
+      XmlElement elem, bool Function(XmlElement) predicate) {
     List<XmlElement> children = [];
     for (final XmlElement child in elem.childElements) {
       if (predicate(child)) {
@@ -143,15 +139,17 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
     return children;
   }
 
-  // Extract the y coordinate from a "dAttribute" - this is the attribute denoting the coordinates of a line element on the staff
+  // Extract the y coordinate from a "d" attribute - this is the attribute denoting the coordinates
+  //   of a line element on the staff.
   // e.g. d="M171.66015625 625.5999999999999L472.4196980046949 625.5999999999999" -- the y coordinate is 625.5999 here
   double _getYCoord(String dAttribute) {
     RegExpMatch regExpMatch = lineCoordinatesRegex.firstMatch(dAttribute)!;
     final y1 = regExpMatch.namedGroup('y1');
-    return double.parse(y1!);
+    if (y1 == null) throw "Null y coordinate in d attribute!";
+    return double.parse(y1);
   }
-  
-  // Get the minimum and maximum y-coordinates of a 'staffline' element. This means getting the 
+
+  // Get the minimum and maximum y-coordinates of a 'staffline' element. This means getting the
   //  minimum and maximum y-coordinates of all staff lines and ledger lines of every measure.
   // e.g. stafflineElement:
   //  <g class="staffline" id="Violin0-1">
@@ -160,13 +158,15 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
   //    ...
   //  </g>
   MinMaxYCoords _getStafflineMinMaxY(XmlElement stafflineElement) {
-    List<XmlElement> measureElements = _getAllChildElements(stafflineElement, _isMeasure);
+    List<XmlElement> measureElements =
+        _getAllChildElements(stafflineElement, _isMeasure);
     if (measureElements.isEmpty) throw "No measures found?!";
 
     var minY = -1.0;
     var maxY = -1.0;
     for (final measureElement in measureElements) {
-      List<XmlElement> measureStaffLines = _getAllChildElements(measureElement, _isLine);
+      List<XmlElement> measureStaffLines =
+          _getAllChildElements(measureElement, _isLine);
       for (final line in measureStaffLines) {
         final double y = _getYCoord(line.getAttribute("d")!);
         if (y < minY || minY == -1.0) minY = y;
@@ -176,14 +176,15 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
     return MinMaxYCoords(minY, maxY);
   }
 
-  // Get the Id of the first measure in the 'staffline' element. See _getStafflineY documentation for
+  // Get the Id of the first measure in the 'staffline' element. See _getStafflineMinMaxY documentation for
   //  example of 'staffline' element. "First" measure means the measure with the lowest Id.
   int _getFirstMeasureId(XmlElement stafflineElement) {
-    List<XmlElement> measureElements = _getAllChildElements(stafflineElement, _isMeasure);
+    List<XmlElement> measureElements =
+        _getAllChildElements(stafflineElement, _isMeasure);
     int minId = -1;
     for (final XmlElement measureElement in measureElements) {
       final idStr = measureElement.getAttribute("id");
-      if (idStr == null) throw "Null measure id";
+      if (idStr == null) throw "Null measure id!";
       final id = int.parse(idStr);
       if (id < minId || minId == -1) minId = id;
     }
@@ -196,16 +197,15 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
     var left = 0;
     var right = groups.length;
     var groupCount = groups.length;
-    
+
     var mid = 0;
 
     while (true) {
-      mid = (left + right ) ~/ 2;
-      if (
-        measureId == groups[mid].startingMeasure ||
-        (measureId > groups[mid].startingMeasure && mid == groupCount - 1) ||
-        (measureId > groups[mid].startingMeasure && measureId < groups[mid + 1].startingMeasure)
-      ) {
+      mid = (left + right) ~/ 2;
+      if (measureId == groups[mid].startingMeasure ||
+          (measureId > groups[mid].startingMeasure && mid == groupCount - 1) ||
+          (measureId > groups[mid].startingMeasure &&
+              measureId < groups[mid + 1].startingMeasure)) {
         return mid;
       } else if (measureId < groups[mid].startingMeasure) {
         right = mid;
@@ -214,13 +214,19 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
       }
     }
   }
- 
+
   void _setupContinuousMode() async {
     final svgXml = await _svgXml;
-    var stafflineElements = svgXml.findAllElements("g").where((line) => line.getAttribute('class') == 'staffline').toList();
+    var stafflineElements = svgXml
+        .findAllElements("g")
+        .where((line) => line.getAttribute('class') == 'staffline')
+        .toList();
     final stafflineElementsCount = stafflineElements.length;
-    final linesPerGroup = svgXml.findAllElements("g").where((line) => (
-      line.getAttribute('class')!.contains('vf-measure') && line.getAttribute('id') == '1')).length;
+    final linesPerGroup = svgXml
+        .findAllElements("g")
+        .where((line) => (line.getAttribute('class')!.contains('vf-measure') &&
+            line.getAttribute('id') == '1'))
+        .length;
     print('linesPerGroup: $linesPerGroup');
     print('stafflineElementsCount: $stafflineElementsCount');
 
@@ -234,26 +240,31 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
         if (minMaxY.maximumY > maxY) maxY = minMaxY.maximumY;
         if (minMaxY.minimumY < minY || minY == -1) minY = minMaxY.minimumY;
       }
-      
+
       print('minY: $minY');
       print('maxY: $maxY');
       final int firstMeasureId = _getFirstMeasureId(stafflineElements[i]);
+      print('measureId: $firstMeasureId');
       groups.add(GroupInfo(firstMeasureId, minY));
     }
 
-    groups.sort((group1Info, group2Info) => group1Info.startingMeasure.compareTo(group2Info.startingMeasure));
+    groups.sort((group1Info, group2Info) =>
+        group1Info.startingMeasure.compareTo(group2Info.startingMeasure));
     final int veryFirstMeasureId = groups[0].startingMeasure;
     for (var groupInfo in groups) {
       groupInfo.startingMeasure -= veryFirstMeasureId;
     }
-    print(groups);
+
+    for (final group in groups) {
+      print('start measure: ${group.startingMeasure}, minY: ${group.minY}');
+    }
   }
 
   void jumpToMeasure(int measureNumber) {
     int groupNumber = _getGroupForMeasure(measureNumber);
-    final double offset = _height * 1/8;
+    final double offset = _height * 1 / 8;
     print('offset $offset');
-    double yCoord = groups[groupNumber].equatorY - offset;
+    double yCoord = groups[groupNumber].minY - offset;
     double maxScroll = _scrollController.position.maxScrollExtent;
     double minScroll = _scrollController.position.minScrollExtent;
 
@@ -277,42 +288,39 @@ class _ContinuousScoreSheetState extends State<ContinuousScoreSheet> {
     _setupSvg();
     _setupContinuousMode();
     _scrollController = ScrollController();
-    _scrollController.addListener((){
-      //print(_scrollController.position.pixels);
+    _scrollController.addListener(() {
+      print(_scrollController.position.pixels);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return OrientationBuilder(
-      builder: (BuildContext context, Orientation orientation) {
-        return Container(
+        builder: (BuildContext context, Orientation orientation) {
+      return Container(
           color: Colors.white,
           child: FutureBuilder(
-            future: _svgs,
-            builder: (BuildContext context, AsyncSnapshot<SvgPicture> snapshot) {
-              if (snapshot.hasData && orientation == Orientation.landscape) {
-                return Scaffold(
-                  body: ListView(
-                    controller: _scrollController,
-                    scrollDirection: Axis.vertical,
-                    children: [snapshot.data!]
-                  ),
-                  floatingActionButton: FloatingActionButton(
-                    onPressed: () {
-                      jumpToMeasure(59 - 1);
-                    },
-                    child: const Icon(Icons.arrow_upward),
+              future: _svgs,
+              builder:
+                  (BuildContext context, AsyncSnapshot<SvgPicture> snapshot) {
+                if (snapshot.hasData && orientation == Orientation.landscape) {
+                  return Scaffold(
+                    body: ListView(
+                        controller: _scrollController,
+                        scrollDirection: Axis.vertical,
+                        children: [snapshot.data!]),
+                    floatingActionButton: FloatingActionButton(
+                      onPressed: () {
+                        jumpToMeasure(75 - 1);
+                      },
+                      child: const Icon(Icons.arrow_upward),
                     ),
-                );
-              } else {
-                return Placeholder();
-              }
-            }
-          )
-        );
-      }
-    );
+                  );
+                } else {
+                  return Placeholder();
+                }
+              }));
+    });
   }
 }
 
@@ -338,13 +346,15 @@ class _PagedScoreSheetState extends State<PagedScoreSheet> {
   void setFile() async {
     // TODO: Turn the file into a backend call. For now, as proof of concept
     // we'll use this file for now.
-    final file = await rootBundle.load('assets/joplin-scott-the-entertainer.pdf');
+    final file =
+        await rootBundle.load('assets/joplin-scott-the-entertainer.pdf');
     setState(() {
       _pdfBytes = file.buffer.asUint8List();
     });
   }
 
-  Future<void> goToRandomPagePerSecond(PDFViewController pdfViewController) async {
+  Future<void> goToRandomPagePerSecond(
+      PDFViewController pdfViewController) async {
     var rng = Random();
     _pdfViewController = pdfViewController;
 
@@ -369,17 +379,19 @@ class _PagedScoreSheetState extends State<PagedScoreSheet> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: _pdfBytes != null ? PDFView(
-        pdfData: _pdfBytes!,
-        swipeHorizontal: true,
-        onViewCreated: goToRandomPagePerSecond,
-      ) : Placeholder(),
+      child: _pdfBytes != null
+          ? PDFView(
+              pdfData: _pdfBytes!,
+              swipeHorizontal: true,
+              onViewCreated: goToRandomPagePerSecond,
+            )
+          : Placeholder(),
     );
   }
 }
 
 class RecorderPage extends StatefulWidget {
-  const RecorderPage({ super.key }); 
+  const RecorderPage({super.key});
 
   @override
   State<RecorderPage> createState() => _RecorderPage();
