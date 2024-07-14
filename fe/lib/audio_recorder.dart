4b9a891a5aafdeeadf6594bca0364d2aebb559d5
Dylex Suan <dylexms@gmail.com>
2024-07-13 20:43:02 -0400

    Re-implement bandpass filter and FFT

--- fe/lib/audio_recorder.dart
+++ fe/lib/audio_recorder.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:audio_session/audio_session.dart';
+import 'package:fftea/impl.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_sound/flutter_sound.dart';
@@ -14,15 +16,17 @@ import 'package:fftea/fftea.dart';
 
 // import 'audio_recorder_io.dart';
 
-const int tSampleRate = 44100;
+const int tSampleRate = 10000;
 typedef _Fn = void Function();
 
+int flag = 0;
+
 int fixedListSize = 2048;
+int lowerFrequency = 50;
+int higherFrequency = 4500;
 // two buffers to allow for possible switching
 List<double> primaryBuffer = List<double>.filled(fixedListSize, 0);
 List<double> secondaryBuffer = List<double>.filled(fixedListSize, 0);
-
-int flag = 0;
 
 class Recorder extends StatefulWidget {
   final void Function(String path) onStop;
@@ -41,6 +45,7 @@ class _RecorderStateRedo extends State<Recorder> {
   RecorderState _mRecorderState = RecorderState.isStopped;
   StreamSubscription? _mRecordingDataSubscription;
   StreamSubscription? _mRecorderSubscription;
+  Radix2FFT fftObj = Radix2FFT(fixedListSize);
 
   int pos = 0;
   double dbLevel = 0;
@@ -90,26 +95,16 @@ class _RecorderStateRedo extends State<Recorder> {
     super.initState();
     // Be careful : openAudioSession return a Future.
     // Do not access your FlutterSoundPlayer or FlutterSoundRecorder before the completion of the Future
-    // _mPlayer!.openPlayer().then((value) {
-    //   setState(() {
-    //     _mPlayerIsInited = true;
-    //   });
-    // });
-
-    // _openRecorder();
     init().then((value) {
       setState(() {
         _mRecorderIsInited = true;
       });
     });
-
-    // _recordSubscription = _mRecorder!.recorderState.listen()
   }
 
   Future<void> init() async {
     await _openRecorder();
     _mRecorderSubscription = _mRecorder!.onProgress!.listen((e) {
-      print("E is printed out: $e");
       setState(() {
         pos = e.duration.inMilliseconds;
         if (e.decibels != null) {
@@ -158,6 +153,35 @@ class _RecorderStateRedo extends State<Recorder> {
     return outputFile.openWrite();
   }
 
+  // band pass filter necessary to remove extraneous noise
+  List<double> implementBandPassFilter(
+      List<double> FFTData, int sampleRate, int lowerFreq, int higherFreq) {
+    List<double> filteredFFTData = FFTData;
+    double fftResolution = sampleRate / (FFTData.length * 2);
+    int lowerBin = lowerFreq ~/ fftResolution;
+    int higherBin = higherFreq ~/ fftResolution;
+
+    for (int i = 0; i < FFTData.length; i++) {
+      if (i < lowerBin || i > higherBin) {
+        filteredFFTData[i] = 0;
+      }
+    }
+
+    return filteredFFTData;
+  }
+
+  List<double> performFFT(Uint8List? streamData) {
+    // use Cooley-Turkey algorithm
+    // FFT separates into two groups: the reals and the conjugates.
+    // Discard the conjugates to get rid of the phase and get the magnitudes.
+    // 1. convert the Uint8List into bytes
+    List<double> streamDataDouble = streamData!.buffer.asFloat32List();
+    // 2. perform the FFT
+    return fftObj.realFft(streamDataDouble).discardConjugates().magnitudes();
+  }
+
+  
+
   Future<void> record() async {
     assert(_mRecorderIsInited && _mPlayer!.isStopped);
     var sink = await createFile();
@@ -167,8 +191,11 @@ class _RecorderStateRedo extends State<Recorder> {
       if (buffer is FoodData) {
         // this is where we will process the PCM data
 
+        List<double> fftResult = performFFT(buffer.data);
+        List<double> fftFiltered = implementBandPassFilter(
+            fftResult, tSampleRate, lowerFrequency, higherFrequency);
         // print("Buffer information: ${buffer.data}!");
-        // print("Buffer length: ${buffer.data.length}");
+        print("Buffer filtered: $fftFiltered");
         sink.add(buffer.data!);
       }
     });
@@ -259,7 +286,6 @@ class _RecorderStateRedo extends State<Recorder> {
           child: SizedBox(width: 56, height: 56, child: icon),
           onTap: () {
             (!(_mRecorder!.isStopped)) ? stopRecorder() : record();
-            // _updateRecordState(_mRecorder!.recorderState);
           },
         ),
       ),
@@ -290,7 +316,6 @@ class _RecorderStateRedo extends State<Recorder> {
           child: SizedBox(width: 56, height: 56, child: icon),
           onTap: () {
             (_mRecorder!.isPaused) ? resumeRecorder() : pauseRecorder();
-            // _updateRecordState(_mRecorder!.recorderState);
           },
         ),
       ),
@@ -349,270 +374,9 @@ class _RecorderStateRedo extends State<Recorder> {
                 _buildText(),
               ],
             ),
-            // if (_amplitude != null) ...[
-            //   const SizedBox(height: 40),
-            //   Text('Current: ${_amplitude?.current ?? 0.0}'),
-            //   Text('Max: ${_amplitude?.max ?? 0.0}'),
-            // ],
           ],
         ),
       ),
     );
   }
 }
-
-// class _RecorderState extends State<Recorder> with AudioRecorderMixin {
-//   late final AudioRecorder _audioRecorder;
-//   Timer? _timer;
-//   int _recordDuration = 0;
-//   RecordState _recordState = RecordState.stop;
-//   StreamSubscription<RecordState>? _recordSub;
-//   StreamSubscription<Amplitude>? _amplitudeSub;
-//   Amplitude? _amplitude;
-
-//   @override
-//   void initState() {
-//     // establish the audio recorder
-//     _audioRecorder = AudioRecorder();
-
-//     _recordSub = _audioRecorder.onStateChanged().listen((recordState) {
-//       _updateRecordState(recordState);
-//     });
-
-//     _amplitudeSub = _audioRecorder
-//         .onAmplitudeChanged(const Duration(milliseconds: 800))
-//         .listen((amp) {
-//       setState(() => _amplitude = amp);
-//     });
-
-//     super.initState();
-//   }
-
-//   Future<void> _start() async {
-//     try {
-//       if (await _audioRecorder.hasPermission()) {
-//         const encoder = AudioEncoder.pcm16bits;
-
-//         if (!await _isEncoderSupported(encoder)) {
-//           return;
-//         }
-
-//         // get all the necessary devices
-//         final devs = await _audioRecorder.listInputDevices();
-//         print("All input devices: ${devs.toString()}");
-
-//         const config =
-//             RecordConfig(encoder: encoder, sampleRate: 10000, numChannels: 1);
-
-//         await recordStream(_audioRecorder, config);
-
-//         _recordDuration = 0;
-
-//         _startTimer();
-//       }
-//     } catch (e) {
-//       print("Error caught: $e");
-//     }
-//   }
-
-//   Future<void> _stop() async {
-//     final path = await _audioRecorder.stop();
-
-//     if (path != null) {
-//       widget.onStop(path);
-
-//       downloadWebData(path);
-//     }
-//   }
-
-//   Future<void> _pause() => _audioRecorder.pause();
-
-//   Future<void> _resume() => _audioRecorder.resume();
-
-//   void _updateRecordState(RecordState recordState) {
-//     setState(() => _recordState = recordState);
-
-//     switch (recordState) {
-//       case RecordState.pause:
-//         _timer?.cancel();
-//         break;
-//       case RecordState.record:
-//         _startTimer();
-//         break;
-//       case RecordState.stop:
-//         _timer?.cancel();
-//         _recordDuration = 0;
-//         break;
-//     }
-//   }
-
-//   Future<bool> _isEncoderSupported(AudioEncoder encoder) async {
-//     final isSupported = await _audioRecorder.isEncoderSupported(
-//       encoder,
-//     );
-
-//     if (!isSupported) {
-//       print("${encoder.name} is not supported on this platform.");
-//       print("Supported encoders are the following: ");
-
-//       for (final e in AudioEncoder.values) {
-//         if (await _audioRecorder.isEncoderSupported(e)) {
-//           print("- ${encoder.name}");
-//         }
-//       }
-//     }
-
-//     return isSupported;
-//   }
-
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: <Widget>[
-//                 _buildRecordStopControl(),
-//                 const SizedBox(width: 20),
-//                 _buildPauseResumeControl(),
-//                 const SizedBox(width: 20),
-//                 _buildText(),
-//               ],
-//             ),
-//             if (_amplitude != null) ...[
-//               const SizedBox(height: 40),
-//               Text('Current: ${_amplitude?.current ?? 0.0}'),
-//               Text('Max: ${_amplitude?.max ?? 0.0}'),
-//             ],
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   // @override
-//   // Widget build(BuildContext context) {
-//   //   return MaterialApp(home: Scaffold( body: Column ( mainAxisAlignment: MainAxisAlignment.center, children: [
-//   //     Row( mainAxisAlignment: MainAxisAlignment.center, children: [
-//   //       Row( mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-//   //         _buildRecordStopControl(),
-//   //         constSizedBox(width: 20),
-//   //         _buildPauseResumeControl(),
-//   //         constSizedBox(width: 20),
-//   //         _buildText(),
-//   //       ],
-//   //       ),
-//   //       if (_amplitude != null) ...[
-//   //         const SizedBox(height: 40),
-//   //         Text('Current: ${_amplitude?.current ?? 0.0}'),
-//   //         Text('Max: ${_amplitude?.max ?? 0.0}'),
-//   //       ],
-//   //     ],
-//   //     ),
-//   //   ),
-//   //   ),
-//   //   );
-//   // }
-
-//   @override
-//   void dispose() {
-//     _timer?.cancel();
-//     _recordSub?.cancel();
-//     _amplitudeSub?.cancel();
-//     _audioRecorder.dispose();
-//     super.dispose();
-//   }
-
-  // Widget _buildRecordStopControl() {
-  //   late Icon icon;
-  //   late Color color;
-
-  //   if (_recordState != RecordState.stop) {
-  //     icon = const Icon(Icons.stop, color: Colors.red, size: 30);
-  //     color = Colors.red.withOpacity(0.1);
-  //   } else {
-  //     final theme = Theme.of(context);
-  //     icon = Icon(Icons.mic, color: theme.primaryColor, size: 30);
-  //     color = theme.primaryColor.withOpacity(0.1);
-  //   }
-
-  //   return ClipOval(
-  //     child: Material(
-  //       color: color,
-  //       child: InkWell(
-  //         child: SizedBox(width: 56, height: 56, child: icon),
-  //         onTap: () {
-  //           (_recordState != RecordState.stop) ? _stop() : _start();
-  //         },
-  //       ),
-  //     ),
-  //   );
-  // }
-
-//   Widget _buildPauseResumeControl() {
-//     if (_recordState == RecordState.stop) {
-//       return const SizedBox.shrink();
-//     }
-
-//     late Icon icon;
-//     late Color color;
-
-//     if (_recordState == RecordState.record) {
-//       icon = const Icon(Icons.pause, color: Colors.red, size: 30);
-//       color = Colors.red.withOpacity(0.1);
-//     } else {
-//       final theme = Theme.of(context);
-//       icon = const Icon(Icons.play_arrow, color: Colors.red, size: 30);
-//       color = theme.primaryColor.withOpacity(0.1);
-//     }
-
-//     return ClipOval(
-//       child: Material(
-//         color: color,
-//         child: InkWell(
-//           child: SizedBox(width: 56, height: 56, child: icon),
-//           onTap: () {
-//             (_recordState == RecordState.pause) ? _resume() : _pause();
-//           },
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _buildText() {
-//     if (_recordState != RecordState.stop) {
-//       return _buildTimer();
-//     }
-
-//     return const Text("Waiting to record");
-//   }
-
-//   Widget _buildTimer() {
-//     final String minutes = _formatNumber(_recordDuration ~/ 60);
-//     final String seconds = _formatNumber(_recordDuration % 60);
-
-//     return Text(
-//       '$minutes : $seconds',
-//       style: const TextStyle(color: Colors.red),
-//     );
-//   }
-
-//   String _formatNumber(int number) {
-//     String numberStr = number.toString();
-//     if (number < 10) {
-//       numberStr = '0$numberStr';
-//     }
-
-//     return numberStr;
-//   }
-
-//   void _startTimer() {
-//     _timer?.cancel();
-
-//     _timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
-//       setState(() => _recordDuration++);
-//     });
-//   }
-// }
