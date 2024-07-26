25dfc71a1f847620362eca097f34d14b0e232673
Dylex Suan <dylexms@gmail.com>
2024-07-26 15:13:14 -0400

    Switch back to record because current version of flutter_sound is malfunctioning

--- fe/lib/audio_recorder.dart
+++ fe/lib/audio_recorder.dart
@@ -15,9 +15,13 @@ import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform
 
 import 'package:path_provider/path_provider.dart';
 // import 'package:permission_handler/permission_handler.dart';
-import 'package:fftea/fftea.dart';
 
-// import 'audio_recorder_io.dart';
+// old version
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:record/record.dart';
+
+import 'audio_recorder_io.dart';
 
 const int tSampleRate = 44100;
 typedef _Fn = void Function();
@@ -118,7 +122,7 @@ class Recorder extends StatefulWidget {
   const Recorder({super.key, required this.onStop});
 
   @override
-  State<Recorder> createState() => _RecorderStateRedo();
+  State<Recorder> createState() => _RecorderState();
 }
 
 class _RecorderStateRedo extends State<Recorder> {
@@ -375,7 +379,7 @@ class _RecorderStateRedo extends State<Recorder> {
       List<double> X_d = downSampleFunc(X, i + 1, finalSizeOfAFHPS);
 
       for (int j = 0; j < finalSizeOfAFHPS; j++) {
-        afHps[i] = afHps[j] * X_d[j];
+        afHps[j] = afHps[j] * X_d[j];
       }
     }
 
@@ -747,5 +751,272 @@ class _RecorderStateRedo extends State<Recorder> {
         ),
       ),
     );
+  }
+}
+
+// import 'dart:async';
+
+// class Recorder extends StatefulWidget {
+//   final void Function(String path) onStop;
+
+//   const Recorder({super.key, required this.onStop});
+
+//   @override
+//   State<Recorder> createState() => _RecorderState();
+// }
+
+class _RecorderState extends State<Recorder> with AudioRecorderMixin {
+  late final AudioRecorder _audioRecorder;
+  Timer? _timer;
+  int _recordDuration = 0;
+  RecordState _recordState = RecordState.stop;
+  StreamSubscription<RecordState>? _recordSub;
+  StreamSubscription<Amplitude>? _amplitudeSub;
+  Amplitude? _amplitude;
+
+  @override
+  void initState() {
+    // establish the audio recorder
+    _audioRecorder = AudioRecorder();
+
+    _recordSub = _audioRecorder.onStateChanged().listen((recordState) {
+      _updateRecordState(recordState);
+    });
+
+    _amplitudeSub = _audioRecorder
+        .onAmplitudeChanged(const Duration(milliseconds: 350))
+        .listen((amp) {
+      setState(() => _amplitude = amp);
+    });
+
+    super.initState();
+  }
+
+  Future<void> _start() async {
+    try {
+      if (await _audioRecorder.hasPermission()) {
+        const encoder = AudioEncoder.pcm16bits;
+
+        if (!await _isEncoderSupported(encoder)) {
+          return;
+        }
+
+        // get all the necessary devices
+        final devs = await _audioRecorder.listInputDevices();
+        print("All input devices: ${devs.toString()}");
+
+        const config =
+            RecordConfig(encoder: encoder, numChannels: 1, sampleRate: 10000);
+
+        await recordStream(_audioRecorder, config);
+
+        _recordDuration = 0;
+
+        _startTimer();
+      }
+    } catch (e) {
+      print("Error caught: $e");
+    }
+  }
+
+  Future<void> _stop() async {
+    final path = await _audioRecorder.stop();
+
+    if (path != null) {
+      widget.onStop(path);
+
+      downloadWebData(path);
+    }
+  }
+
+  Future<void> _pause() => _audioRecorder.pause();
+
+  Future<void> _resume() => _audioRecorder.resume();
+
+  void _updateRecordState(RecordState recordState) {
+    setState(() => _recordState = recordState);
+
+    switch (recordState) {
+      case RecordState.pause:
+        _timer?.cancel();
+        break;
+      case RecordState.record:
+        _startTimer();
+        break;
+      case RecordState.stop:
+        _timer?.cancel();
+        _recordDuration = 0;
+        break;
+    }
+  }
+
+  Future<bool> _isEncoderSupported(AudioEncoder encoder) async {
+    final isSupported = await _audioRecorder.isEncoderSupported(
+      encoder,
+    );
+
+    if (!isSupported) {
+      print("${encoder.name} is not supported on this platform.");
+      print("Supported encoders are the following: ");
+
+      for (final e in AudioEncoder.values) {
+        if (await _audioRecorder.isEncoderSupported(e)) {
+          print("- ${encoder.name}");
+        }
+      }
+    }
+
+    return isSupported;
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildRecordStopControl(),
+                const SizedBox(width: 20),
+                _buildPauseResumeControl(),
+                const SizedBox(width: 20),
+                _buildText(),
+              ],
+            ),
+            if (_amplitude != null) ...[
+              const SizedBox(height: 40),
+              Text('Current: ${_amplitude?.current ?? 0.0}'),
+              Text('Max: ${_amplitude?.max ?? 0.0}'),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(home: Scaffold( body: Column ( mainAxisAlignment: MainAxisAlignment.center, children: [
+  //     Row( mainAxisAlignment: MainAxisAlignment.center, children: [
+  //       Row( mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+  //         _buildRecordStopControl(),
+  //         constSizedBox(width: 20),
+  //         _buildPauseResumeControl(),
+  //         constSizedBox(width: 20),
+  //         _buildText(),
+  //       ],
+  //       ),
+  //       if (_amplitude != null) ...[
+  //         const SizedBox(height: 40),
+  //         Text('Current: ${_amplitude?.current ?? 0.0}'),
+  //         Text('Max: ${_amplitude?.max ?? 0.0}'),
+  //       ],
+  //     ],
+  //     ),
+  //   ),
+  //   ),
+  //   );
+  // }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _recordSub?.cancel();
+    _amplitudeSub?.cancel();
+    _audioRecorder.dispose();
+    super.dispose();
+  }
+
+  Widget _buildRecordStopControl() {
+    late Icon icon;
+    late Color color;
+
+    if (_recordState != RecordState.stop) {
+      icon = const Icon(Icons.stop, color: Colors.red, size: 30);
+      color = Colors.red.withOpacity(0.1);
+    } else {
+      final theme = Theme.of(context);
+      icon = Icon(Icons.mic, color: theme.primaryColor, size: 30);
+      color = theme.primaryColor.withOpacity(0.1);
+    }
+
+    return ClipOval(
+      child: Material(
+        color: color,
+        child: InkWell(
+          child: SizedBox(width: 56, height: 56, child: icon),
+          onTap: () {
+            (_recordState != RecordState.stop) ? _stop() : _start();
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPauseResumeControl() {
+    if (_recordState == RecordState.stop) {
+      return const SizedBox.shrink();
+    }
+
+    late Icon icon;
+    late Color color;
+
+    if (_recordState == RecordState.record) {
+      icon = const Icon(Icons.pause, color: Colors.red, size: 30);
+      color = Colors.red.withOpacity(0.1);
+    } else {
+      final theme = Theme.of(context);
+      icon = const Icon(Icons.play_arrow, color: Colors.red, size: 30);
+      color = theme.primaryColor.withOpacity(0.1);
+    }
+
+    return ClipOval(
+      child: Material(
+        color: color,
+        child: InkWell(
+          child: SizedBox(width: 56, height: 56, child: icon),
+          onTap: () {
+            (_recordState == RecordState.pause) ? _resume() : _pause();
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildText() {
+    if (_recordState != RecordState.stop) {
+      return _buildTimer();
+    }
+
+    return const Text("Waiting to record");
+  }
+
+  Widget _buildTimer() {
+    final String minutes = _formatNumber(_recordDuration ~/ 60);
+    final String seconds = _formatNumber(_recordDuration % 60);
+
+    return Text(
+      '$minutes : $seconds',
+      style: const TextStyle(color: Colors.red),
+    );
+  }
+
+  String _formatNumber(int number) {
+    String numberStr = number.toString();
+    if (number < 10) {
+      numberStr = '0$numberStr';
+    }
+
+    return numberStr;
+  }
+
+  void _startTimer() {
+    _timer?.cancel();
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() => _recordDuration++);
+    });
   }
 }
--- fe/lib/audio_recorder_io.dart
+++ fe/lib/audio_recorder_io.dart
@@ -8,228 +8,322 @@ import 'package:path_provider/path_provider.dart';
 
 import 'package:record/record.dart';
 
-// scidart package used for preprocessing functions...
+import 'package:fftea/fftea.dart';
+import 'package:fftea/impl.dart';
 
-import 'package:scidart/src/numdart/numdart.dart';
-import 'package:scidart/src/scidart/signal/signal.dart';
-import 'package:scidart/src/scidart/fftpack/fft/fft.dart';
-import 'package:scidart/src/scidart/fftpack/rfft/rfft.dart';
-import 'package:scidart/src/scidart/fftpack/rfft/rifft.dart';
+class Tuple<T, U> {
+  T x;
+  U y;
+  Tuple(this.x, this.y);
+}
+
+double twelfthRootOf2 = pow(2, 1.0 / 12).toDouble();
+
+double freqForNote(String? baseNote, int noteIndex) {
+  double A4 = 440.0;
+
+  Map<String, double> baseNotesFreq = {
+    "A2": A4 / 4,
+    "A3": A4 / 2,
+    "A4": A4,
+    "A5": A4 * 2,
+    "A6": A4 * 4
+  };
+
+  Map<String, double> scaleNotes = {
+    "C": -9.0,
+    "C#": -8.0,
+    "D": -7.0,
+    "D#": -6.0,
+    "E": -5.0,
+    "F": -4.0,
+    "F#": -3.0,
+    "G": -2.0,
+    "G#": -1.0,
+    "A": 1.0,
+    "A#": 2.0,
+    "B": 3.0,
+    "Cn": 4.0
+  };
+
+  List<int> scaleNotesIndex = List<int>.filled(14, 0);
+
+  for (int i = -9; i < 5; i++) {
+    scaleNotesIndex[i + 9] = i;
+  }
+
+  int noteIndexValue = scaleNotesIndex[noteIndex];
+
+  double? freq0 = baseNotesFreq[baseNote];
+
+  double freq = freq0! * pow(twelfthRootOf2, noteIndexValue).toDouble();
+
+  return freq;
+}
+
+List<Tuple<String, double>> getAllNotesFreq() {
+  List<Tuple<String, double>> orderedNoteFreq =
+      List<Tuple<String, double>>.empty(growable: true);
+  List<String> orderedNotes = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B"
+  ];
+
+  for (int octaveIndex = 2; octaveIndex < 7; octaveIndex++) {
+    String baseNote = "A${octaveIndex}";
+    for (int noteIndex = 0; noteIndex < 12; noteIndex++) {
+      double noteFrequency = freqForNote(baseNote, noteIndex);
+      String noteName = "${orderedNotes[noteIndex]}$octaveIndex";
+      orderedNoteFreq.add(Tuple(noteName, noteFrequency));
+    }
+  }
+
+  return orderedNoteFreq;
+}
+
+List<Tuple<String, double>> orderedNoteFreq = getAllNotesFreq();
 
 mixin AudioRecorderMixin {
+  final numberOfFFTBins = 8192;
+  final sampleRate = 10000;
+
   Future<void> recordFile(AudioRecorder recorder, RecordConfig config) async {
     final path = await _getPath();
 
     await recorder.start(config, path: path);
   }
 
-  List<double> normalizeAudio(List<int> samples) {
-    List<double> normalized = [];
-    for (int sample in samples) {
-      double normalizedSample = sample / 32768.0;
-      normalized.add(normalizedSample);
-    }
+  List<double> findPeaks(List<double> arr, int threshold) {
+    var N = arr.length - 2;
+    var ix = List<double>.empty(growable: true);
+    var ax = List<double>.empty(growable: true);
 
-    return normalized;
-  }
-
-  double computeYinPitch(List<double> audioBuffer, int sampleRate) {
-    int tauEstimate = 0;
-    double minThreshold = 0.1; // Adjust threshold as needed
-    int bufferSize = audioBuffer.length;
-
-    // Compute difference function
-    List<double> difference = List.filled(bufferSize, 0);
-    for (int tau = 1; tau < bufferSize; tau++) {
-      for (int i = 0; i < bufferSize - tau; i++) {
-        difference[tau] += (audioBuffer[i] - audioBuffer[i + tau]).abs();
+    for (int i = 1; i < N; i++) {
+      if (arr[i - 1] <= arr[i] && arr[i] >= arr[i + 1] && arr[i] > threshold) {
+        ix.add(i.toDouble());
+        ax.add(arr[i].toDouble());
       }
     }
 
-    // Compute cumulative mean normalized difference function (CMND)
-    List<double> cumulativeMean = List.filled(bufferSize, 0);
-    cumulativeMean[1] = 1.0; // Avoid division by zero
-    double runningSum = 0;
-    for (int tau = 1; tau < bufferSize; tau++) {
-      runningSum += difference[tau];
-      cumulativeMean[tau] = difference[tau] / (runningSum / tau);
+    return ix;
+  }
+
+  List<double> implementBandPassFilter(List<double> FFTData, int sampleRate,
+      int numberOfBins, int lowerFreq, int higherFreq) {
+    List<double> filteredFFTData = FFTData;
+    double fftResolution = sampleRate / numberOfBins;
+    print("FFT resolution: ${fftResolution}");
+    int lowerBin = lowerFreq ~/ fftResolution;
+    int higherBin = higherFreq ~/ fftResolution;
+
+    print("Lower bin: ${lowerBin}");
+    print("Higher bin: ${higherBin}");
+    for (int i = 0; i < FFTData.length; i++) {
+      if (i < lowerBin || i > higherBin) {
+        filteredFFTData[i] = 0;
+      }
     }
 
-    // Find minimum tau for which CMND is below threshold
-    for (int tau = 2; tau < bufferSize; tau++) {
-      if (cumulativeMean[tau] < minThreshold) {
-        tauEstimate = tau;
+    return filteredFFTData;
+  }
+
+  List<double> hanningFunction(int size) {
+    List<double> result = List<double>.filled(size, 0);
+    for (int i = 0; i < result.length; i++) {
+      result[i] = 0.5 - 0.5 * cos((2 * pi * i) / (size - 1));
+    }
+    return result;
+  }
+
+  double rmsSignalThreshold(List<double> chunk) {
+    double sum = 0;
+    for (int i = 0; i < chunk.length; i++) {
+      sum += pow(chunk[i], 2).toDouble();
+    }
+    double mean = sum / chunk.length;
+    print("Mean: $mean");
+    return sqrt(mean);
+  }
+
+  List<double> downSampleFunc(List<double> X, int decimation, int lengthOfRet) {
+    // when you downsample, you simply take the ith sample in the original array
+    List<double> retX = List<double>.filled(lengthOfRet, 0);
+    for (int i = 0; i < lengthOfRet; i++) {
+      retX[i] = X[i * decimation];
+    }
+
+    return retX;
+  }
+
+  double noteThresholdScaledByHPS(double buffer_rms) {
+    double noteThreshold = 500 * (4 / 0.090) * buffer_rms;
+    return noteThreshold;
+  }
+
+  List<int> argWhere(List<double> X, double threshold) {
+    List<int> retArgs = List<int>.filled(X.length, 0);
+
+    int numArgsAdded = 0;
+    for (int i = 0; i < X.length; i++) {
+      if (X[i] > threshold) {
+        retArgs[numArgsAdded] = i;
+        numArgsAdded += 1;
+      }
+    }
+    return retArgs.sublist(0, numArgsAdded);
+  }
+
+  List<double> normalizedData(List<double> X) {
+    List<double> retData = List<double>.filled(X.length, 0);
+    for (int i = 0; i < retData.length; i++) {
+      retData[i] = X[i] / 32768;
+    }
+    return retData;
+  }
+
+  String findNearestNote(
+      List<Tuple<String, double>> orderedNoteFreq, int freq) {
+    String finalNoteName = "note_not_found";
+    double lastDistance = 1000000.0;
+    for (int i = 0; i < orderedNoteFreq.length; i++) {
+      double currentDistance = (orderedNoteFreq[i].y - freq).abs();
+      if (currentDistance < lastDistance) {
+        lastDistance = currentDistance;
+        finalNoteName = orderedNoteFreq[i].x;
+      } else if (currentDistance > lastDistance) {
         break;
       }
     }
-
-    // Calculate pitch (in Hz)
-    double pitchInHz = sampleRate / tauEstimate.toDouble();
-    return pitchInHz;
+    return finalNoteName;
   }
 
-  String pitchToNote(double pitchInHz) {
-    // Implement logic to map pitch frequencies to musical notes
-    // Example implementation
-    print("Pitch in hz: $pitchInHz");
-    List<String> notes = [
-      'C',
-      'C#',
-      'D',
-      'D#',
-      'E',
-      'F',
-      'F#',
-      'G',
-      'G#',
-      'A',
-      'A#',
-      'B'
-    ];
-    int noteIndex = (12 * (log(pitchInHz / 440) / log(2))).round() % 12;
-    print("Note index given: $noteIndex\r");
-    return notes[noteIndex];
-  }
+  List<Tuple<int, double>> pitchSpectralHPS(List<double> X, double rms) {
+    int iOrder = 4;
+    print("Length of X: ${X.length}");
+    int finalSizeOfAFHPS = ((numberOfFFTBins / 2) - 1) ~/ iOrder;
+    print("Final size of AFHPS: $finalSizeOfAFHPS");
+    double fMin = 65.41;
 
-  void processAudioFrame(List<double> audioFrame, int sampleRate) {
-    double pitch = computeYinPitch(audioFrame, sampleRate);
-    String detectedNote = pitchToNote(pitch);
-    print('Detected note: $detectedNote\r');
+    int kMin = (fMin / sampleRate * 2 * (X.length - 1)).round();
+    print("kMin given: $kMin");
+
+    List<double> afHps = X.sublist(0, finalSizeOfAFHPS);
+
+    for (int i = 1; i < iOrder; i++) {
+      List<double> X_d = downSampleFunc(X, i + 1, finalSizeOfAFHPS);
+
+      for (int j = 0; j < finalSizeOfAFHPS; j++) {
+        afHps[j] = afHps[j] * X_d[j];
+      }
+    }
+
+    // print("afHPS after: $afHps");
+    double noteThreshold = noteThresholdScaledByHPS(rms);
+
+    // print("AFHPS: ${afHps}");
+    List<double> rawFreqValues = afHps.sublist(kMin);
+    List<int> allFreqs = argWhere(rawFreqValues, noteThreshold);
+
+    List<int> freqsOut = List<int>.filled(allFreqs.length, 0);
+
+    for (int i = 0; i < freqsOut.length; i++) {
+      freqsOut[i] =
+          ((allFreqs[i] + kMin) / (X.length - 1) * sampleRate / 2).toInt();
+    }
+
+    print("All frequencies: $allFreqs");
+    print("Frequency out: $freqsOut");
+
+    List<Tuple<int, double>> freqsOutTmp =
+        List<Tuple<int, double>>.empty(growable: true);
+
+    for (int i = 0; i < allFreqs.length; i++) {
+      Tuple<int, double> indexValue = Tuple(freqsOut[i], rawFreqValues[i]);
+      freqsOutTmp.add(indexValue);
+    }
+
+    return freqsOutTmp;
+    // List<int> allFreqs = argWhere(afHps.sublist(kMin), noteThreshold);
   }
 
   Future<void> recordStream(AudioRecorder recorder, RecordConfig config) async {
     final path = await _getPath();
     final file = File(path);
 
-    const numberOfFFTBins = 2048;
-    const thresholdFFT =
-        200000; // set the threshold as a base, then test the different harmonics
-
-    // band pass filter settings (in Hz)
-    const fftResolution = 10000 / numberOfFFTBins;
-
-    const lowBoundFilter = 50;
-    const highBoundFilter = 4100;
-
-    final hannWindow = hann(numberOfFFTBins);
-
     final stream = await recorder.startStream(config);
 
     final stopWatch = Stopwatch();
+
+    List<int>? primaryDataBuffer;
+    List<int>? secondaryDataBuffer;
+    int numBuffers = 2;
+
+    Radix2FFT fftObj = Radix2FFT(numberOfFFTBins);
+
+    int flag = 1;
 
     stream.listen(
       (data) {
         stopWatch.start();
         // Convert the bytes into a 16 bit integer PCM array as convention
-        final rawListIntPCM =
-            recorder.convertBytesToInt16(Uint8List.fromList(data));
+        var rawListIntPCM;
 
-        // print("Size of list: ${rawListIntPCM.length}");
+        if (flag == 1) {
+          primaryDataBuffer =
+              recorder.convertBytesToInt16(Uint8List.fromList(data));
+          rawListIntPCM = primaryDataBuffer;
+        } else {
+          secondaryDataBuffer =
+              recorder.convertBytesToInt16(Uint8List.fromList(data));
+          rawListIntPCM = secondaryDataBuffer;
+        }
+
+        flag = (flag + 1) % numBuffers;
+
+        var windowedResult = List<double>.filled(rawListIntPCM.length, 0);
+
+        var hann = hanningFunction(rawListIntPCM.length);
+
+        for (int i = 0; i < rawListIntPCM.length; i++) {
+          windowedResult[i] = rawListIntPCM[i].toDouble() / 32768 * hann[i];
+        }
 
         var fftList = List<double>.filled(numberOfFFTBins, 0);
 
-        for (int i = 0; i < rawListIntPCM.length; i++) {
-          fftList[i] = rawListIntPCM[i].toDouble();
+        for (int i = 0; i < windowedResult.length; i++) {
+          fftList[i] = windowedResult[i].toDouble();
         }
 
-        // var normalizedAudioList = normalizeAudio(rawListIntPCM);
+        file.writeAsStringSync(fftList.toString(), mode: FileMode.append);
 
-        // processAudioFrame(normalizedAudioList, 20000);
-        // YIN ALGORITHM
+        Float64List FFTResult =
+            fftObj.realFft(fftList).discardConjugates().magnitudes();
 
-        // windowing operation
-        var sciListDoublePCM = Array(fftList);
-        var windowedPCM = sciListDoublePCM * hannWindow;
+        List<double> FFTFiltered = implementBandPassFilter(
+            FFTResult, sampleRate, numberOfFFTBins, 50, 4500);
 
-        // Perform FFT on the complex field
-        // var complexArray = arrayToComplexArray(windowedPCM);
-        // var fftResult = fft(complexArray, n: numberOfFFTBins);
+        double rmsThreshold = rmsSignalThreshold(windowedResult);
 
-        var absFFTResult =
-            arrayComplexAbs(rfft(windowedPCM, n: numberOfFFTBins));
+        print("RMS Threshold: $rmsThreshold");
+        // print("Peaks: $peaks");
 
-        // Obtain the FFT result, plot on Octave
-        // var absFFTResult = arrayComplexAbs(fftResult);
+        List<Tuple> pHPS = pitchSpectralHPS(FFTFiltered, rmsThreshold);
 
-        for (var i = 0; i < absFFTResult.length; i++) {
-          if (i * fftResolution < lowBoundFilter ||
-              i * fftResolution > highBoundFilter) {
-            // print("I index: $i");
-            // absFFTResult[i] = Complex(real: 0, imaginary: 0);
-            absFFTResult[i] = 0;
-          }
+        for (int i = 0; i < pHPS.length; i++) {
+          String noteName = findNearestNote(orderedNoteFreq, pHPS[i].x);
+          print(
+              "=> Freq: ${pHPS[i].x}  Hz value: ${pHPS[i].y}  Note name: $noteName");
         }
-
-        // var ifftResult = rifft(absFFTResult);
-
-        // print("Result of ifft result: $ifftResult");
-
-        // List<int> preprocessing = [];
-
-        // for (int i = 0; i < ifftResult.length; i++) {
-        //   preprocessing.add(ifftResult[i].toInt());
-        // }
-
-        // print("Preprocessing array: $preprocessing");
-        // var normalizedAudioList = normalizeAudio(preprocessing);
-
-        // processAudioFrame(normalizedAudioList, 10000);
-        //---------------------------------------------------------
-        //
-        // FFT IMPLEMENTATION
-        //
-        // ---------------------------------------------------------
-        // var [bins, mags] = findPeaks(absFFTResult);
-
-        var [bins, mags] =
-            findPeaks(absFFTResult, threshold: thresholdFFT.toDouble());
-
-        if (bins.length == 0) {
-          print("No new notes detected.");
-          return;
-        }
-
-        if (bins.length > 6) {
-          print("Multiple overtones heard, delaying input signal...");
-          return;
-        }
-
-        // Choose the lowest peak and determine the fundamental frequency
-        var firstPeakBin = bins[0].toInt();
-        var firstPeakMag = mags[0].toInt();
-
-        var lowerFirstPeakBin = firstPeakBin - 1;
-        var higherFirstPeakBin = firstPeakBin + 1;
-
-        var lowerMagFirstPeakBin = absFFTResult[lowerFirstPeakBin].toDouble();
-        var higherMagFirstPeakBin = absFFTResult[higherFirstPeakBin].toDouble();
-
-        print("Lower first peak bin: $lowerFirstPeakBin");
-        print("Higher first peak bin: $higherFirstPeakBin");
-
-        print("Bins printed: $bins");
-        print("Magnitudes printed: $mags");
-
-        print("Bin below peak: ${absFFTResult[lowerFirstPeakBin.toInt()]}");
-        print("Bin above peak: ${absFFTResult[higherFirstPeakBin.toInt()]}");
-
-        // Interpolation time!
-
-        var H = sqrt(firstPeakMag);
-        var L = lowerMagFirstPeakBin < higherMagFirstPeakBin
-            ? sqrt(lowerMagFirstPeakBin)
-            : sqrt(higherMagFirstPeakBin);
-        var D = H / (L + H);
-
-        print("First peak bin: $firstPeakBin");
-        print("FFT Resolution: $fftResolution");
-        var freq = (firstPeakBin + D) * fftResolution;
-
-        print("H index: $H");
-        print("L index: $L");
-        print("D index: $D");
-        print("Estimated frequency: $freq");
-
         // stopWatch.stop();
         // print("Elapsed time: ${stopWatch.elapsedMilliseconds}");
         // stopWatch.reset();
