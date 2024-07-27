91afe2046185e338487ccadfc8aa10a9994c49a9
Barry Zhang <45883553+bluebarryz@users.noreply.github.com>
2024-07-27 10:09:34 -0400

    Lift compiled mxl to global app state (#39)
    * Get pitch differences from compiled mxl
    
    * Move compiled mxl json value to global state
    
    * Update pubsec
    
    * add didChangeDependencies call

---
+++ fe/lib/backend_helpers.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart' show rootBundle;
+
+// TODO: add argument for the MXL file to be parsed
+Future<http.Response> compileMxl() async {
+  const uri = "http://localhost:4000";
+  const score = "emerald_moonlight";
+  final request = http.MultipartRequest('POST', Uri.parse('$uri/compile-mxl'));
+
+  final musicxmlBytes =
+      (await rootBundle.load('assets/$score.mxl')).buffer.asUint8List();
+  request.files.add(
+      http.MultipartFile.fromBytes('file', musicxmlBytes, filename: score));
+
+  final streamResponse = await request.send();
+  final response = await http.Response.fromStream(streamResponse);
+
+  return response;
+}
